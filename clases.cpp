d8450a4c1baed0a8b1f284ccb0cb4b586591b925
HP <jfait@fie.undef.edu.ar>
2022-05-13 18:41:50 -0300

    ADD - Header and source files

---
+++ clases.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HP on 13/5/2022.
+//
+
+#include "clases.h"
---
+++ clases.h
@@ -0,0 +1,8 @@
+//
+// Created by HP on 13/5/2022.
+//
+
+#ifndef TEMPLATE_CLASES_H
+    #define TEMPLATE_CLASES_H
+
+#endif //TEMPLATE_CLASES_H
